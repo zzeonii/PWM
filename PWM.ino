17da045b348e4693936405ee1660176ac9c339ab
zzeonii <129237950+zzeonii@users.noreply.github.com>
2023-03-30 17:43:51 +0900

    Update PWM.ino

--- PWM.ino
+++ PWM.ino
@@ -7,12 +7,13 @@ void setup() {
   Timer1.initialize();
   Timer1.pwm(LED,0);
 
-  Timer1.setPeriod(1000000); //1Hz
-  Timer1.setPwmDuty(LED,511); // 0~1023
-
+  Timer1.setPeriod(1000); //1Hz
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  for(int t_high=0;t_high<=1023;t_high++) {
+    Timer1.setPwmDuty(LED,t_high); // 0~1023
+    delay(1);
+  }
 }
